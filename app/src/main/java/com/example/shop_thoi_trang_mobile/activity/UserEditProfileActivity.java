bd2188a121bf7df1d65726351759d99554f8f07b
KrissTiNa207 <99896326+TriSBie@users.noreply.github.com>
2024-07-02 12:36:48 +0700

    Upload missing files

--- app/src/main/java/com/example/shop_thoi_trang_mobile/activity/UserEditProfileActivity.java
+++ app/src/main/java/com/example/shop_thoi_trang_mobile/activity/UserEditProfileActivity.java
@@ -1,51 +1,17 @@
 package com.example.shop_thoi_trang_mobile.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.shop_thoi_trang_mobile.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class UserProfileActivity extends AppCompatActivity {
+public class UserEditProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent = null;
-                if (item.getItemId() == R.id.nav_home) {
-                    // Chuyển sang activity Home (ví dụ)
-                    intent = new Intent(UserProfileActivity.this, HomeActivity.class);
-                } else if (item.getItemId() == R.id.nav_cart) {
-                    // Chuyển sang activity Category (ví dụ)
-                    intent = new Intent(UserProfileActivity.this, CartActivity.class);
-                } else if (item.getItemId() == R.id.nav_noti) {
-                    // Chuyển sang activity Cart (ví dụ)
-                    intent = new Intent(UserProfileActivity.this, HomeActivity.class);
-                } else if (item.getItemId() == R.id.nav_profile) {
-                    // Chuyển sang activity Profile (ví dụ)
-                    intent = new Intent(UserProfileActivity.this, UserProfileActivity.class);
-                }
-                if (intent != null) {
-                    startActivity(intent);
-                    return true;
-                }
-                return false;
-            }
-        });
+        setContentView(R.layout.activity_edit_profile);
     }
-
 }
--- app/src/main/java/com/example/shop_thoi_trang_mobile/activity/UserProfileActivity.java
+++ app/src/main/java/com/example/shop_thoi_trang_mobile/activity/UserProfileActivity.java
@@ -1,2 +1,49 @@
-package com.example.shop_thoi_trang_mobile.activity;public class UserProfileActivity {
+package com.example.shop_thoi_trang_mobile.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shop_thoi_trang_mobile.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class UserProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_profile);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent = null;
+                if (item.getItemId() == R.id.nav_home) {
+                    // Chuyển sang activity Home (ví dụ)
+                    intent = new Intent(UserProfileActivity.this, HomeActivity.class);
+                } else if (item.getItemId() == R.id.nav_cart) {
+                    // Chuyển sang activity Category (ví dụ)
+                    intent = new Intent(UserProfileActivity.this, CartActivity.class);
+                } else if (item.getItemId() == R.id.nav_noti) {
+                    // Chuyển sang activity Cart (ví dụ)
+                    intent = new Intent(UserProfileActivity.this, HomeActivity.class);
+                } else if (item.getItemId() == R.id.nav_profile) {
+                    // Chuyển sang activity Profile (ví dụ)
+                    intent = new Intent(UserProfileActivity.this, UserProfileActivity.class);
+                }
+                if (intent != null) {
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
 }
